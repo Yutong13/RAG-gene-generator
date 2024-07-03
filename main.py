cc8abe3106c0cd321b177f5c494fb3c98d8feda0
Yutong13 <yutong13@ualberta.ca>
2024-07-03 14:40:44 +0800

    remove . from icd code output

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from code_generator import predict_icd_code
 def main():
     query = input("Query for PubMed: ") # ask for search query
     icd_code = predict_icd_code(query) # list of ICD code
+    for i in range(len(icd_code)):
+        icd_code[i] = "".join(icd_code[i].split("."))
     print(icd_code)
-
+    
 main()
