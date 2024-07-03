e5c6b3e166cf6f44e7ecd02b7c1c0e997ee4925b
Yutong13 <yutong13@ualberta.ca>
2024-07-03 14:57:49 +0800

    implmenet code search

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from code_generator import predict_icd_code
+import pandas as pd
 
 def main():
     query = input("Query for PubMed: ") # ask for search query
     icd_code = predict_icd_code(query) # list of ICD code
     for i in range(len(icd_code)):
         icd_code[i] = "".join(icd_code[i].split("."))
-    print(icd_code)
+    print("ICD codes: ",icd_code)
+
+    df = pd.read_csv("Section111ValidICD10-Jan2024.csv")
+    names = []
+    for code in icd_code:
+        icd_row = df[df['CODE'] == code] # search for ICD code
+        names.append(icd_row.iloc[:, 1].to_string()) # Names in Short is second column in scv
+    print("Names:\n", names)
     
 main()
